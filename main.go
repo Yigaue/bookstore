382c65ea62c4104ada49b5506a346bde139f215e
Yigaue <lekiagospel@gmail.com>
2024-02-20 16:39:34 +0100

    update getBook, postBook handlers and create deleteBook handler

--- main.go
+++ main.go
@@ -4,16 +4,26 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/yigaue/bookstore/database"
 	"github.com/yigaue/bookstore/models"
 )
 
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.GET("/books/:id", getBook)
+	router.POST("/books", postBooks)
+	router.DELETE("/books/:id", deleteBook)
+	router.Run("localhost:8080")
+}
+
 // getBooks responds with the list of all books as json
 func getBooks(c *gin.Context) {
-	var db *sql.DB
-	db = database.DBConnect()
+	// var db *sql.DB
+	db := database.DBConnect()
 	var books []models.Book
 
 	rows, err := db.Query("SELECT * FROM book")
@@ -37,32 +47,59 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
-func main() {
-	router := gin.Default()
-	router.GET("/books", getBooks)
-	router.GET("/books/:id", getBook)
-	router.POST("/books", postBooks)
-	router.Run("localhost:8080")
-}
-
 func getBook(c *gin.Context) {
+	db := database.DBConnect()
 	id := c.Param("id")
-	for _, book := range books {
-		if book.ID == id {
-			c.IndentedJSON(http.StatusOK, book)
-			return
-		}
+	var book models.Book
+
+	row := db.QueryRow("SELECT * FROM book WHERE id = ?", id)
+	err := row.Scan(&book.ID, &book.Author, &book.Title, &book.Price)
+
+	if err == sql.ErrNoRows {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+  }
+
+	if err != nil {
+		fmt.Errorf("book ID, %d: %v", id, err)
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+
+	c.IndentedJSON(http.StatusOK, book)
 }
 
 func postBooks(c *gin.Context) {
+	db := database.DBConnect()
 	var newBook models.Book
-
 	if err := c.BindJSON(&newBook); err != nil {
 		return
 	}
+	row, err := db.Exec("INSERT INTO book (title, author, price) VALUES (?, ?, ?)", newBook.Title, newBook.Author, newBook.Price)
+	if err != nil {
+		fmt.Errorf("postBooks %v", err)
+	}
 
-	books = append(books, newBook)
+	id, err := row.LastInsertId()
+
+	if err != nil {
+		fmt.Errorf("error getting lastID: %v", err)
+	}
+
+	newBook.ID = strconv.Itoa(int(id))
+
 	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func deleteBook(c *gin.Context) {
+	id := c.Param("id")
+	db := database.DBConnect()
+	_, err := db.Exec("DELETE FROM book WHERE id = ?", id)
+
+	if err != nil {
+		fmt.Errorf("deleteBooks %v", err)
+	}
+
+	if err != nil {
+		fmt.Errorf("Error getting lastInsertedId deleteBook %v", err)
+	}
+
+	c.IndentedJSON(http.StatusOK, "Book deleted successful")
 }
